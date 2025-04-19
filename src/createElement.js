9fa5d23175d62d201e25ef12c6a92b9e3c88e81b
David Pirek <dpirek@gmail.com>
2025-04-19 08:31:30 -0400

    adding view more jquery style methods

--- src/createElement.js
+++ src/createElement.js
@@ -40,5 +40,77 @@ export function createElement(tag, props) {
     element.parentNode.removeChild(element);
   };
 
+  element.text = function(text) {
+    if (text) {
+      element.innerText = text;
+    } else {
+      return element.innerText;
+    }
+  }
+
+  element.html = function(html) {
+    if (html) {
+      element.innerHTML = html;
+    } else {
+      return element.innerHTML;
+    }
+  }
+
+  element.attr = function(name, value) {
+    if (value) {
+      element.setAttribute(name, value);
+    } else {
+      return element.getAttribute(name);
+    }
+  }
+
+  element.append = function(child) {
+    if (Array.isArray(child)) {
+      child.forEach(c => element.appendChild(c));
+    } else {
+      element.appendChild(child);
+    }
+  }
+
+  element.prepend = function(child) {
+    if (Array.isArray(child)) {
+      child.forEach(c => element.insertBefore(c, element.firstChild));
+    } else {
+      element.insertBefore(child, element.firstChild);
+    }
+  }
+
+  element.click = function(handler) {
+    element.addEventListener('click', handler);
+  }
+
+  element.on = function(event, handler) {
+    element.addEventListener(event, handler);
+  }
+
+  element.class = function(className) {
+    if (className) {
+      element.classList.add(className);
+    } else {
+      return element.className;
+    }
+  }
+
+  element.removeClass = function(className) {
+    if (className) {
+      element.classList.remove(className);
+    }
+  }
+
+  element.toggleClass = function(className) {
+    if (className) {
+      element.classList.toggle(className);
+    }
+  }
+
+  element.hasClass = function(className) {
+    return element.classList.contains(className);
+  }
+
   return element;
 }
